01e120c6843f0a7980a7e52ca61f24eed40a1bdd
awadb3223 <awadb3223@HRH-IC0027318.wrdsb.ca>
2018-01-11 10:15:06 -0500

    Working out Bodies

--- core/src/com/mygdx/game/MainClass.java
+++ core/src/com/mygdx/game/MainClass.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.FPSLogger;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -17,100 +18,78 @@ import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class MainClass extends ApplicationAdapter {
 
-    SpriteBatch batch;
-    //gravity vector
-    Vector2 gravity;
-    //variables for lights
-    private World world;
-    private Stage stage;
-    private Box2DDebugRenderer debugRenderer;
-    private RayHandler rayHandler;
-
+    OrthographicCamera camera;
+    World world;
+    float width, height;
+    FPSLogger logger;
+    Box2DDebugRenderer renderer;
+    Body circleBody;
+    
+    
+    
     @Override
     public void create() {
-        Gdx.app.setLogLevel(Application.LOG_DEBUG);
-
-        gravity = new Vector2(0, 0);
-
-        world = new World(gravity, false);
-
-        batch = new SpriteBatch();
-
-        //BodyDef
-        BodyDef bodyDef = new BodyDef();
-        bodyDef.type = BodyType.StaticBody;
-        bodyDef.position.set(50, 250);
-
-        //create a new body
-        Body body = world.createBody(bodyDef);
-
-
-        Gdx.input.setInputProcessor(stage);
-        float ratio = (float) (Gdx.graphics.getWidth()) / (float) (Gdx.graphics.getHeight());
-
-        stage = new Stage(new ScreenViewport());
-        stage.getCamera().position.set(0, 0, 10);
-        stage.getCamera().lookAt(0, 0, 0);
-        stage.getCamera().viewportWidth = 10;
-        stage.getCamera().viewportHeight = 10 / ratio;
-        debugRenderer = new Box2DDebugRenderer();
-
-        rayHandler = new RayHandler(world);
-        rayHandler.setAmbientLight(0.1f, 0.1f, 0.1f, 1f);
-        rayHandler.setBlurNum(3);
-
-        //POINT LIGHTS
-        //PointLight pl2 = new PointLight(rayHandler, 128, Color.BLUE, 10, 5, 2);
-        //PointLight pl = new PointLight(rayHandler, 128, Color.RED, 10, -5, 2);
-
-        //DIRECTIONAL LIGHTS
-        //DirectionalLight p3 = new DirectionalLight(rayHandler, 20, Color.YELLOW, 90);
-
-        //CONE LIGHTS
-        ConeLight p4 = new ConeLight(rayHandler, 5, Color.YELLOW, 5, -5, 0, 0, 45);
-        //turns on light cone
-        p4.setStaticLight(true);
-        // softens the light so its less harsh
-        p4.setSoft(false);
-
-        ConeLight p5 = new ConeLight(rayHandler, 5, Color.BLUE, 5, 5, 0, 180, 45);
-        p5.setStaticLight(true);
-        p5.setSoft(false);
-
-        p4.setSoftnessLength(0);
-        p5.setSoftnessLength(0);
-
-        //enables shadows
-        rayHandler.setShadows(true);
-        //turns on point light
-//        pl.setStaticLight(false);
-//        pl.setSoft(true);
+        
+        width = Gdx.graphics.getWidth() / 5;
+        height = Gdx.graphics.getHeight() / 5;
+        
+        camera = new OrthographicCamera (width, height);
+        camera.position.set(width * 0.5f, height * 0.5f, 0);
+        camera.update();
+        
+        world = new World(new Vector2(0,-9.8f), false);
+        
+        renderer = new Box2DDebugRenderer();
+        
+        logger = new FPSLogger();
+        
+        //DYNAMIC BODY
+        BodyDef circleDef = new BodyDef();
+        circleDef.type = BodyType.DynamicBody;
+        circleDef.position.set(width / 2f, height / 2f);
+        
+        circleBody = world.createBody(circleDef);
+        
+        CircleShape circleShape = new CircleShape();
+        circleShape.setRadius(3f);
+        
+        FixtureDef circleFixture = new FixtureDef();
+        circleFixture.shape = circleShape;
+        circleFixture.density = 0.04f;
+        circleFixture.friction = 0.02f;
+        circleFixture.restitution = 0.08f;
+        
+        circleBody.createFixture(circleFixture);
+        
+        //STATIC BODY A.K.A. THE GROUND
+        BodyDef groundBodyDef = new BodyDef();
+        groundBodyDef.position.set(0,3);
+        
+        Body groundBody = world.createBody(groundBodyDef);
+        
+        PolygonShape groundBox = new PolygonShape();
+        groundBox.setAsBox(camera.viewportWidth * 2, 3.0f);
+        
+        groundBody.createFixture(groundBox, 0.0f);
+        
     }
 
     @Override
     public void render() {
-        Gdx.gl.glClearColor(0f, 0f, 0f, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        stage.act();
-        world.step(Gdx.graphics.getDeltaTime(), 6, 2);
-        stage.draw();
-
-        debugRenderer.render(world, stage.getCamera().combined);
-
-        rayHandler.setCombinedMatrix(stage.getCamera().combined, 0, 0, 1, 1);
-        rayHandler.updateAndRender();
+      
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        rayHandler.dispose();
+     world.dispose();
     }
 }
