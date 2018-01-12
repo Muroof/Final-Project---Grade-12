30f5a2ba65d532599a73297d4caae4729abe7154
awadb3223 <awadb3223@HRH-IC0027318.wrdsb.ca>
2018-01-12 09:44:53 -0500

    GOT THE BALL TO WORK. will test out shadows

--- core/src/com/mygdx/game/MainClass.java
+++ core/src/com/mygdx/game/MainClass.java
@@ -10,6 +10,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.FPSLogger;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -27,12 +28,14 @@ import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class MainClass extends ApplicationAdapter {
 
-    OrthographicCamera camera;
-    World world;
-    float width, height;
-    FPSLogger logger;
-    Box2DDebugRenderer renderer;
-    Body circleBody;
+    private OrthographicCamera camera;
+    private World world;
+    private float width, height;
+    private FPSLogger logger;
+    private Box2DDebugRenderer renderer;
+    private Body circleBody;
+    
+    private RayHandler handler;
     
     
     
@@ -46,7 +49,7 @@ public class MainClass extends ApplicationAdapter {
         camera.position.set(width * 0.5f, height * 0.5f, 0);
         camera.update();
         
-        world = new World(new Vector2(0,-9.8f), false);
+        world = new World(new Vector2(0,-50f), false);
         
         renderer = new Box2DDebugRenderer();
         
@@ -55,7 +58,7 @@ public class MainClass extends ApplicationAdapter {
         //DYNAMIC BODY
         BodyDef circleDef = new BodyDef();
         circleDef.type = BodyType.DynamicBody;
-        circleDef.position.set(width / 2f, height / 2f);
+        circleDef.position.set(50, 50);
         
         circleBody = world.createBody(circleDef);
         
@@ -64,7 +67,7 @@ public class MainClass extends ApplicationAdapter {
         
         FixtureDef circleFixture = new FixtureDef();
         circleFixture.shape = circleShape;
-        circleFixture.density = 0.04f;
+        circleFixture.density = 2f;
         circleFixture.friction = 0.02f;
         circleFixture.restitution = 0.8f;
         
@@ -81,12 +84,14 @@ public class MainClass extends ApplicationAdapter {
         
         groundBody.createFixture(groundBox, 0.0f);
         
+        //CONE LIGHTS
+        
     }
 
     @Override
     public void render() {
       Gdx.gl.glClearColor(0, 0, 0, 1);
-      Gdx.gl.glClear(0);
+      Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
       
       renderer.render(world, camera.combined);
       
@@ -98,5 +103,6 @@ public class MainClass extends ApplicationAdapter {
     @Override
     public void dispose() {
      world.dispose();
+     
     }
 }
