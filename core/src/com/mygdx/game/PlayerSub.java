2a4d63df6956865911aa75a4268a65a4ea47c41e
awadb3223 <awadb3223@HRH-IC0027318.wrdsb.ca>
2018-01-16 10:05:05 -0500

    Created Character superclass with subclasses. finna get movement done tonight hopefully, unless i have a chem lab due the next day

---
+++ core/src/com/mygdx/game/PlayerSub.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mygdx.game;
+
+import com.badlogic.gdx.physics.box2d.World;
+
+/**
+ *
+ * @author awadb3223
+ */
+public class PlayerSub extends CharacterSuper{
+    
+    private World world;
+   
+    
+    public PlayerSub(float x, float y){
+        super(x, y);
+        
+    }
+    
+    
+    
+    
+}
